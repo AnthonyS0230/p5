5ff6f69ebba4f85de12ef54f2dbb4340a7a82e19
Anthonys0230 <anthony0230@icloud.com>
2024-10-14 13:15:58 -0500

    Initial Commit

---
+++ p5/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  p5
+//
+//  Created by Anthony Salazar on 10/14/24.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+
+{
+    int j = 0; //row
+    int i = 0; //column
+    int size = 0;
+    sin >> size;
+    
+    std::cout << " Enter the size " << std::endl;
+    while (i < size)
+        j = 0;
+    while ( j > i)
+       
+        return 0;
+}
+
